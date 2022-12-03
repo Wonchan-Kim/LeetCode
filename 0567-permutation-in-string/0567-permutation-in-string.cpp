75408d59a173c89d6fba0c85973205f45f5566bc
Wonchan-Kim <81521208+Wonchan-Kim@users.noreply.github.com>
2022-12-03 13:57:21 +0900

    Time: 8 ms (87.72%), Space: 7.2 MB (72.28%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,33 +1,40 @@
+
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
+private:
+    bool is_equal(int a[],int b[]){
+        for(int i=0;i<26;i++)
+            if(a[i]!=b[i]) return 0;
+        return 1;
     }
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c: s1) freqS1[c-'a']++;
+        int cnt1[26]={0};    // for string s1
+        for(int i=0;i<s1.size();i++)
+            cnt1[s1[i]-'a']++;      // mapping s1 elements with count1 array
         
-        vector<int> freqS2(26, 0);
-        int i=0, j=0;
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
-            }
+        // now traverse s2 in window size of s1.size() and compare
+        int cnt2[26]={0};    // for string s2-> window
+        int windowsize=s1.size();
+        // checking for 1st window
+        int i=0;
+        while( i<windowsize && i<s2.size() ) // i<s2.size() -> taki i,s2 ki length se aage na nikal jae ; window s2 ki length se aage na nikal jae ; s1.size()>s2.size()
+        {
+            cnt2[s2[i]-'a']++;
+            i++;
         }
-        return false;
+        if( is_equal(cnt1,cnt2) ) 
+            return 1;   // permutation present
+        
+        // for next windows
+        while( i<s2.length() ){
+            int add = s2[i]-'a';
+            cnt2[add]++;
+            int remove = s2[i-windowsize]-'a';
+            cnt2[remove]--;
+            if( is_equal(cnt1,cnt2) )  return 1;
+            i++;
+        }
+        return 0;  // s2 ki size tak traversed but can't find
+//we r comparing 1st window seperately as at time of first window we can't delete (i-windowsize)th element
     }
 };
