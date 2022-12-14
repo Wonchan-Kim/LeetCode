d95a3cf7ee06115abe4310660bf7b2848df38770
Wonchan-Kim <81521208+Wonchan-Kim@users.noreply.github.com>
2022-12-14 10:08:28 +0900

    Time: 6 ms (90.86%), Space: 9.2 MB (10.04%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,27 +1,21 @@
 class Solution {
-public:
+public:int helper(vector<vector<int>>& triangle, int i, int j,vector<vector<int>>& dp,int n)
+{
+    if(i==n-1)
+    {
+        return triangle[i][j];
+    }
+    if(dp[i][j]!=-1)
+    {
+        return dp[i][j];
+    }
+    int move1=triangle[i][j]+helper(triangle,i+1,j,dp,n);
+    int move2=triangle[i][j]+helper(triangle,i+1,j+1,dp,n);
+    return dp[i][j]=min(move1,move2);
+}
     int minimumTotal(vector<vector<int>>& triangle) {
-         auto height = triangle.size()-1;
-        if(height == 0)
-            return triangle[0][0];
-        while(height >= 2){
-            for(int j = 0; j < triangle[height-1].size(); j++){
-                triangle[height-1][j] += min(triangle[height][j], triangle[height][j+1]);
-            }
-            height--;
-        }
-        return triangle[0][0]+ min(triangle[1][0], triangle[1][1]);
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return helper(triangle,0,0,dp,n);
     }
 };
-
-/*
-트리의 높이 = traingle.size();
-트리 갱신
-i와 i-1이 있을 때,
-for(int j = 0; j < traingle[i-1].size(); j++){
-    triangle[i-1][j] += min(traingle[i][j], traingle[i][j+1]);
-얼만큼 반복하냐
-while(높이 > 0)
-
-}
-*/               
