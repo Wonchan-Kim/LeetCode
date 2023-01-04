589207ad6ec04efbecadecb51ac75f1bd9e3f4f3
Wonchan-Kim <81521208+Wonchan-Kim@users.noreply.github.com>
2023-01-04 12:10:20 +0900

    Time: 35 ms (15.32%), Space: 17.5 MB (69.11%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -28,17 +28,13 @@ public:
             for(int i = 0; i < size; i++){
                 Node* node = q.front();
                 q.pop();
-                
                 if(i < size - 1)
                     node -> next = q.front();
                 if(node->left)
                     q.push(node->left);
                 if(node->right)
                     q.push(node->right);
-                
-                
-            }
-            
+            }   
         }
         return root;
     }
